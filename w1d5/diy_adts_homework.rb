1474dd113c3dfa97a542a732a55657bcba54daba
fmbf <circumnavigate.the.globe@gmail.com>
2017-12-07 21:52:26 -0800

    Create DIY_ADTs homework file

---
+++ w1d5/diy_adts_homework.rb
@@ -0,0 +1,25 @@
+# Exercise 1 - Stack
+
+# Let's write a Stack class. To do this, use the following framework:
+# To test that your code works,
+# create a new instance of the Stack class,
+# and play around with adding and removing elements.
+# Remember, a stack follows the principle of LIFO!
+
+  class Stack
+    def initialize
+      # create ivar to store stack here!
+    end
+
+    def add(el)
+      # adds an element to the stack
+    end
+
+    def remove
+      # removes one element from the stack
+    end
+
+    def show
+      # return a copy of the stack
+    end
+  end
