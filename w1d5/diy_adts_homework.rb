6bb048c22a46d0ba56029427b1114d503639f66d
fmbf <circumnavigate.the.globe@gmail.com>
2017-12-07 22:43:15 -0800

    Implement Map class

--- w1d5/diy_adts_homework.rb
+++ w1d5/diy_adts_homework.rb
@@ -55,3 +55,50 @@ class Queue
     @queue
   end
 end
+
+
+# Exercise 3 - Map
+#
+# As you saw in the videos for tonight,
+# a map can be implemented using a 2D array.
+# Let's write a Map class
+# (following a similar pattern to Stack and Queue)
+# that implements a map using only arrays.
+
+# Remember, a map stores information in key-value pairs,
+# where the keys are always unique.
+# When implemented with arrays, a map might look something like this:
+# my_map = [[k1, v1], [k2, v2], [k3, v2], ...].
+#
+# Our Map class should have the following instance methods:
+# assign(key, value), lookup(key), remove(key), show.
+# Note that the assign method can be used to either
+# create a new key-value pair or update the value for a pre-existing key.
+# It's up to you to check whether a key currently exists!
+#
+# As always, test your code thoroughly
+# to make sure all properties of maps are enforced.
+
+
+class Map
+  def initialize
+    @map = Array.new(0) {[]}
+  end
+
+  def assign(key, value)
+    @map << [key, value] unless lookup(key)
+  end
+
+  def lookup(key)
+    @map.each {|(k, v)| return v if k == key}
+    nil
+  end
+
+  def remove(key)
+    @map.reject! {|(k, v)| k == key}
+  end
+
+  def show
+    @map
+  end
+end
