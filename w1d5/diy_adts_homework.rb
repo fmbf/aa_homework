8fd49bdf73fd8ae73bfdb6e31e7442aa9467e461
fmbf <circumnavigate.the.globe@gmail.com>
2017-12-07 22:04:49 -0800

    Implement Stack and Queue classes

--- w1d5/diy_adts_homework.rb
+++ w1d5/diy_adts_homework.rb
@@ -6,20 +6,52 @@
 # and play around with adding and removing elements.
 # Remember, a stack follows the principle of LIFO!
 
-  class Stack
-    def initialize
-      # create ivar to store stack here!
-    end
-
-    def add(el)
-      # adds an element to the stack
-    end
-
-    def remove
-      # removes one element from the stack
-    end
-
-    def show
-      # return a copy of the stack
-    end
+class Stack
+  def initialize
+    # create ivar to store stack here!
+    @stack = Array.new
   end
+
+  def add(el)
+    # adds an element to the stack
+    @stack << el
+  end
+
+  def remove
+    # removes one element from the stack
+    @stack.pop
+  end
+
+  def show
+    # return a copy of the stack
+    @stack
+  end
+end
+
+
+# Exercise 2 - Queue
+#
+# Now let's write a Queue class.
+# We will need the following instance methods:
+# enqueue(el), dequeue, and show.
+#
+# Test your code to ensure it follows the principle of FIFO.
+
+
+class Queue
+  def initialize
+    @queue = Array.new
+  end
+
+  def enqueue(el)
+    @queue.unshift(el)
+  end
+
+  def dequeue
+    @queue.pop
+  end
+
+  def show
+    @queue
+  end
+end
