85b55fc9c4aa08105629343013100ba039649db9
fmbf <circumnavigate.the.globe@gmail.com>
2017-12-19 23:35:24 -0800

    First rails project homework

---
+++ w3d2/first_rr_project/db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+# stark = House.create(name: "Stark")
+# lannister = House.create(name: "Lannister")
+# targaryen = House.create(name: "Targaryen")
+#
+#
+#
+# eddard = Person.create(name: "Eddard Stark", house_id: stark)
+# tyrion = Person.create(name: "Tyrion Lannister", house_id: lannister)
+# danny = Person.create(name: "Daenerys Targaryen", house_id: targaryen)
